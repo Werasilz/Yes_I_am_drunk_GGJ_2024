2cdbc0532e8a41f4aefcd4f7dd0ead37153261d9
Zenzz <zen_frame@hotmail.com>
2024-01-27 03:00:37 +0700

    UI Window

---
+++ Assets/Scripts/UI/UIWindow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RievelGame
+{
+    public class UIWindow : MonoBehaviour
+    {
+        public string windowName;
+        public bool isOpen;
+
+        public void Open()
+        {
+            isOpen = true;
+            gameObject.SetActive(isOpen);
+        }
+
+        public void Close()
+        {
+            isOpen = false;
+            gameObject.SetActive(isOpen);
+        }
+    }
+}
