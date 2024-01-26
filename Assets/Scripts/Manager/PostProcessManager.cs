cb54114ccaf530f115c39c307532b498a15399fe
Zenzz <zen_frame@hotmail.com>
2024-01-27 04:26:41 +0700

    Post Process Manager

--- Assets/Scripts/Manager/PostProcessManager.cs
+++ Assets/Scripts/Manager/PostProcessManager.cs
@@ -1,28 +1,66 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using DG.Tweening;
+
+[System.Serializable]
+public class ProfileAnimate
+{
+    public float target;
+    public float duration;
+}
 
 public class PostProcessManager : SceneSingleton<PostProcessManager>
 {
+    [Header("Volume")]
     [SerializeField] private Volume _postProcessVolume;
-    [SerializeField] private float _chromaticAberrationIntensity = 1.0f;
-    [SerializeField] private float _lensDistortionIntensity = 1.0f;
+
+    [Header("Profile Animate")]
+    [SerializeField] private ProfileAnimate _chromaticAberrationIntensity;
+    [SerializeField] private ProfileAnimate _lensDistortionIntensity;
+
+    private ChromaticAberration _chromaticAberration;
+    private LensDistortion _lensDistortion;
 
     protected override void Awake()
     {
         base.Awake();
     }
 
-    void Start()
+    private void Start()
     {
-        if (_postProcessVolume.profile.TryGet<ChromaticAberration>(out var _chromaticAberration))
+        if (_postProcessVolume.profile.TryGet<ChromaticAberration>(out _chromaticAberration))
         {
-            _chromaticAberration.intensity.value = _chromaticAberrationIntensity;
         }
 
-        if (_postProcessVolume.profile.TryGet<LensDistortion>(out var _lensDistortion))
+        if (_postProcessVolume.profile.TryGet<LensDistortion>(out _lensDistortion))
         {
-            _lensDistortion.intensity.value = _lensDistortionIntensity;
         }
+    }
+
+    [ContextMenu("SetChromaticAberration")]
+    public void SetChromaticAberration()
+    {
+        float value = _chromaticAberration.intensity.value;
+        DOTween.To(() => value, x => value = x, _chromaticAberrationIntensity.target, _chromaticAberrationIntensity.duration).OnUpdate(() =>
+        {
+            _chromaticAberration.intensity.value = value;
+        }).OnComplete(() =>
+        {
+            _chromaticAberration.intensity.value = _chromaticAberrationIntensity.target;
+        });
+    }
+
+    [ContextMenu("SetLensDistortion")]
+    public void SetLensDistortion()
+    {
+        float value = _lensDistortion.intensity.value;
+        DOTween.To(() => value, x => value = x, _lensDistortionIntensity.target, _lensDistortionIntensity.duration).OnUpdate(() =>
+        {
+            _lensDistortion.intensity.value = value;
+        }).OnComplete(() =>
+        {
+            _lensDistortion.intensity.value = _lensDistortionIntensity.target;
+        });
     }
 }
